c91ea1a11d19d1a0bc10dd0f31182c062553414d
AndriiMandziuk <edwardthomas7770@gmail.com>
2025-08-16 06:23:10 +0200

    update db

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "127.0.0.1",
@@ -13,17 +14,18 @@ const pool = mysql.createPool({
 (async () => {
   try {
     const createTableSQL = `
-      CREATE TABLE IF NOT EXISTS recipes (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-        making_time VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-        serves VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-        ingredients VARCHAR(300) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-        cost INT NOT NULL,
-        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-      )
+        CREATE TABLE IF NOT EXISTS recipes (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            making_time VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            serves VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            ingredients VARCHAR(300) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            cost INT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at DATETIME NOT NULL
+        )
     `;
+
     await pool.execute(createTableSQL);
     console.log("Recipes table is ready.");
   } catch (err) {
